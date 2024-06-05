9bf687111a7899b8795f475169e5342cd67515bf
ANDRÉ BARCELOS MICHELS DE MATTOS <andre_b_mattos@estudante.sesisenai.org.br>
2024-06-05 20:17:51 -0300

    install heroku for deploy

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require("cors")
+require("dotenv/config")
 
 
 
@@ -12,6 +13,6 @@ app.use(express.json())
 app.use(routes)
 app.use(cors)
 
-const PORT = 3333
+const PORT = process.env.PORT  || 3333
 app.listen(PORT, () => console.log(`Server is runing on Port ${PORT}`)
 )
